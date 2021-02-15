5976a83ccec512cd9438dfede20be6f4d945f5ee
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-02-15 22:16:49 +0100

    style: accept rustfmt suggestions, structs3

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -24,11 +24,15 @@ impl Package {
     }
 
     fn is_international(&self) -> bool {
-        if self.sender_country != self.recipient_country { true } else { false }
+        if self.sender_country != self.recipient_country {
+            true
+        } else {
+            false
+        }
     }
 
     fn get_fees(&self, cents_per_gram: i32) -> i32 {
-        self.weight_in_grams * cents_per_gram 
+        self.weight_in_grams * cents_per_gram
     }
 }
 
