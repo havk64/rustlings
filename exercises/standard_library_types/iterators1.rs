d87f53758d00b4c99e84a25d2c6ddae9f9bf23b7
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-02-22 20:25:50 +0100

    style: apply rustfmt on exercise 1 on iterators

--- exercises/standard_library_types/iterators1.rs
+++ exercises/standard_library_types/iterators1.rs
@@ -1,14 +1,14 @@
 // iterators1.rs
-// 
+//
 //  Make me compile by filling in the `???`s
 //
 // When performing operations on elements within a collection, iterators are essential.
-// This module helps you get familiar with the structure of using an iterator and 
+// This module helps you get familiar with the structure of using an iterator and
 // how to go through elements within an iterable collection.
-// 
+//
 // Execute `rustlings hint iterators1` for hints :D
 
-fn main () {
+fn main() {
     let my_fav_fruits = vec!["banana", "custard apple", "avocado", "peach", "raspberry"];
 
     let mut my_iterable_fav_fruits = my_fav_fruits.iter();
